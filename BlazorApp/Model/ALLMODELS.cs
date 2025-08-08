97f3bc3c5f55892cc6aaefd21332f02f9a0962b2
Muhammad Anas Amin <anas.ccab40@gmail.com>
2025-08-08 10:17:02 +0500

    Admin Dashboard

--- BlazorApp/Model/ALLMODELS.cs
+++ BlazorApp/Model/ALLMODELS.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorApp.Model
 {
@@ -64,7 +65,57 @@ namespace BlazorApp.Model
     }
 
 
+    public class Booking
+    {
+
+        public int BookingId { get; set; }
+        public DateTime StartTime { get; set; } = DateTime.Now;
+        public DateTime? ExitTime { get; set; }
+        public int? DurationHours { get; set; }
+        public decimal? TotalCost { get; set; }
+        public string? Status { get; set; }
+        public bool IsPaid { get; set; } = false;
+        public int UserId { get; set; }
+        public Users? User { get; set; }
+        public int ParkingLocationId { get; set; }
+        public ParkingLocation? ParkingLocation { get; set; }
+        public int SlotId { get; set; }
+        public ParkingSlot? ParkingSlot { get; set; }
+    }
 
 
+    public class Users
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public bool Active { get; set; }
+        public int RoleId { get; set; }
+        public Role? Role { get; set; }
+
+    }
+
+
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string? Roles { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+
+    }
+
+    public class ParkingSlot
+    {
+        public int SlotId { get; set; }
+        public string? SlotNumber { get; set; }
+        public string? SlotType { get; set; }
+        public int? PricePerHour { get; set; } = null;
+        public int ParkingLocationId { get; set; }
+        public ParkingLocation? ParkingLocation { get; set; }
+    }
 
 }
--- BlazorApp/Model/ApiResponse.cs
+++ BlazorApp/Model/ApiResponse.cs
@@ -13,4 +13,30 @@ namespace BlazorApp.Model
         [JsonPropertyName("value")]
         public int Value { get; set; }
     }
+    public class CountResponseOfIncome
+    {
+        //[JsonPropertyName("value")]
+        public string Value { get; set; }
+    }
+
+    public class FullResponse
+    {
+        public int Value { get; set; }
+        public List<object> Formatters { get; set; }
+        public List<object> ContentTypes { get; set; }
+        public object DeclaredType { get; set; }
+        public int StatusCode { get; set; }
+    }
+
+    public class CountResponseWrapper
+    {
+        [JsonPropertyName("value")]
+        public decimal Value { get; set; }
+
+        public List<object> Formatters { get; set; }
+        public List<string> ContentTypes { get; set; }
+        public object DeclaredType { get; set; }
+        public int StatusCode { get; set; }
+    }
+
 }
